6a6d0797431c0f2c71a2a0d2908f76e66f8ecada
OmarcorzO <omarcorzo2014@outlook.com>
2025-02-14 17:12:50 -0500

    Avance

--- src/pages/instrument/Instrument.tsx
+++ src/pages/instrument/Instrument.tsx
@@ -14,6 +14,43 @@ const Instrument = () => {
       </div>
       <div>¿Cuántas personas dependen económicamente de usted?</div>
       <div>¿Posee acceso a internet en su hogar?</div>
+      <div>¿Cuenta con un dispositivo electronico para estudiar?</div>
+      <div>¿Con quién vive actualmente?</div>
+      <div>¿Cuál es el nivel educativo más alto alcanzado por su padre?</div>
+      <div>¿Cuál es el nivel educativo más alto alcanzado por su madre?</div>
+      <div>¿Tiene hijos a su cargo?</div>
+      <div>¿Cómo describe la relación con su familia?</div>
+      <div>¿Alguien en su familia le apoya emocionalmente en sus estudios?</div>
+      <div>¿Cuál es su rango de edad?</div>
+      <div>¿Cuál es su estado civil?</div>
+      <div>¿Tiene alguna condición de salud especial o discapacidad?</div>
+      <div>¿Cuál es su lugar de residencia?</div>
+      <div>
+        ¿Qué medio de transporte utiliza con mayor frecuencia para llegar a la
+        universidad?
+      </div>
+      <div>¿La carrera que estaba estudiando era de su interés personal?</div>
+      <div>¿Cómo califica su experiencia de aprendizaje?</div>
+      <div>
+        ¿Qué tan útil considera que es la carrera estudiada con su actual
+        proyecto de vida y profesional?
+      </div>
+      <div>
+        ¿Cuáles fueron las principales dificultades académicas que enfrentó
+        mientras estudiaba?
+      </div>
+      <div>
+        ¿Considera que recibió suficiente apoyo académico por parte de la
+        universidad?
+      </div>
+      <div>¿Cómo califica la infraestructura física de la institución?</div>
+      <div>
+        ¿Cómo evalúa la calidad de la conectividad a internet en la institución?
+      </div>
+      <div>
+        ¿Qué percepción tiene sobre la calidad del servicio ofrecido por el
+        personal administrativo?
+      </div>
     </FullLayout>
   );
 };
